d6649dcd3ac04935b72ac971544227961e1b27bc
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2021-10-13 17:47:43 +0200

    add combo

--- Poker/src/v1/Combo.java
+++ Poker/src/v1/Combo.java
@@ -1,5 +1,8 @@
 package v1;
 
 public abstract class Combo {
-
+	String name;
+	int priorityValue;
+	
+	public abstract String toString();
 }
---
+++ Poker/src/v1/Hauteur.java
@@ -0,0 +1,16 @@
+package v1;
+
+public class Hauteur extends Combo{
+
+	public Hauteur(String newName, int valuePriority) {
+		this.name = newName;
+		this.priorityValue = valuePriority;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
