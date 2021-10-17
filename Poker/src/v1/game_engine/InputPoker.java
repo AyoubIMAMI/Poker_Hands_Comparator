ab9ce32b6e90b964bba98f33732fa4c22db2ac03
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2021-10-17 17:24:40 +0200

    update input to carry out processing of color and all business rule

--- Poker/src/v1/game_engine/InputPoker.java
+++ Poker/src/v1/game_engine/InputPoker.java
@@ -10,70 +10,67 @@ import v1.game_class.Hand;
 
 //this class manage the input of cards and init the game
 public class InputPoker {
+	int valueOfTheCard = 0;
+	int colorOfTheCard = 1;
 
-	public InputPoker(){}
+	public InputPoker() {
+	}
 
-	//create a game
+	// create a game
 	public Game init() {
-		int nbrCarte= 3;
+		int nbrCarte = 3;
 		Hand hand1 = new Hand("player1", promptCard(nbrCarte));
 		Hand hand2 = new Hand("player2", promptCard(nbrCarte));
 		return new Game(hand1, hand2);
 	}
-	
-	//catch a user input to create a card
+
+	// catch a user input to create a card
 	// accept only numbers between 1 and 13 and only numbers no strings
 	@SuppressWarnings("resource")
-	public ArrayList<Card> promptCard(int nbrCarte){
+	public ArrayList<Card> promptCard(int nbrCarte) {
 
 		ArrayList<Card> allCard = new ArrayList<Card>();
 		Scanner myObj = new Scanner(System.in);
-		Scanner myObjC = new Scanner(System.in);
 
-    	System.out.println("Entrer la valeur puis la coulaur des cartes du joueurs:");
+		System.out.println(
+				"Entrer la valeur (de 1 à 13) puis la couleur des cartes (ca = carreau, co = coeur, pi = pique, tr = trèfle) du joueurs (exemple: 5 pi):");
 
-		for(int i = 0 ; i < nbrCarte ; i++) {
-			int cardValue= 0;
-		    while(!(cardValue >0 && cardValue <14)) {
+		for (int i = 0; i < nbrCarte; i++) {
+			int cardValue = 0;
+			String cardColor = "";
 
-		    	System.out.print("card value: ");
+			while (!(cardValue > 0 && cardValue < 14) || !(cardColor.equals("ca") || cardColor.equals("co")
+					|| cardColor.equals("pi") || cardColor.equals("tr"))) {
+				System.out.print("write a card: ");
 
-		    	if(!(myObj.hasNextInt())){
-		    		printErrorValue();
-		    		myObj.next();
-		    	}
+				String[] promptValue;
+				String onlyDigit = "-?\\d+";
+				promptValue = myObj.nextLine().split(" ");
+				boolean integerOrNot1 = promptValue[0].matches(onlyDigit); // check si c'est une chaine de caractère
 
-		    	else {
-		    		cardValue = myObj.nextInt();
-		    		if(!(cardValue >0 && cardValue <14)) {
-			    		printErrorValue();
-			    	}
-		    	}
-	    	}
-
-			System.out.println("Card color:");
-			while(!(myObjC.hasNext("Pi") || myObjC.hasNext("Ca") ||myObjC.hasNext("Co") ||myObjC.hasNext("Tr"))){
-				printErrorColor();
-				System.out.println("Card color:");
-				myObjC.next();
-
+				if (integerOrNot1) {
+					cardValue = Integer.valueOf(promptValue[0]);
+					if (cardValue > 0 && cardValue < 14 && promptValue.length > 1) {
+						cardColor = promptValue[1];
+						if (!(cardColor.equals("ca") || cardColor.equals("co") || cardColor.equals("pi")
+								|| cardColor.equals("tr")))
+							printError();
+					} else
+						printError();
+				} else
+					printError();
 			}
-			String cardColor = myObjC.next();
 
 			allCard.add(new Card(cardValue, cardColor));
 		}
-    	System.out.println(""); // \n
+		System.out.println(""); // \n
 		Collections.sort(allCard);
-	    return allCard;
-	}
-	
-	private void printErrorValue() {
-		System.out.println("");
-		System.out.println("Mauvaise valeur entree, la valeur d'une carte est comprise entre 1 et 13!");
+		return allCard;
 	}
 
-	private void printErrorColor() {
-		System.out.println("");
-		System.out.println("Mauvaise couleur entree, elle doit etre l'une des suivantes : Pi, Ca, Co, Tr");
+	private void printError() {
+		System.out.println(
+				"Attention, la valeur d'une carte est comprise entre 1 et 13 et les couleurs disponibles sont ca = carreau, co = coeur, pi = pique, tr = trèfle!");
 	}
+
 }
